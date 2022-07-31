2d5bbb615042267670bcf7840ff48a7e70c3e804
Vladislav <Vladislav_lebedkin@mail.ru>
2022-07-31 13:26:16 +0300

    [Инициализация сайта] сделал кастет сайта  [01]

---
+++ src/index.js
@@ -0,0 +1,6 @@
+import '../src/styles/style.scss';
+import Sign from "./pages/Sign";
+
+const root = document.getElementById('root');
+console.log(root);
+root.innerHTML = Sign;
---
+++ src/pages/Sign/index.js
@@ -0,0 +1,5 @@
+import './Sign.scss'
+import Sign from './Sign.hbs';
+
+const as = 'neenenenenenee'
+export default Sign({a: as});
